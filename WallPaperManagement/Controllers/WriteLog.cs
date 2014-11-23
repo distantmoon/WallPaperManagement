2eb5f9750fcaf39f8a2d1989150fe79592cbea23
distantmoon <distantmoon@126.com>
2014-11-23 14:22:07 +0800

    First Commit

---
+++ WallPaperManagement/Controllers/WriteLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WallPaperManagement.Controllers
+{
+    public class WriteLog
+    {
+        public static void Write(string log)
+        {
+        }
+    }
+}
